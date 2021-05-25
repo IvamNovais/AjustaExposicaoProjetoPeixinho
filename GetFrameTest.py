2c86fd39534e7913194f6205778b7839bd91b1f3
IvamNovais <72990399+IvamNovais@users.noreply.github.com>
2021-05-25 11:43:33 -0300

    Update GetFrameTest.py

--- GetFrameTest.py
+++ GetFrameTest.py
@@ -6,4 +6,4 @@ tirarFim = 15 #tempo em segundos que sera removido no fim do video
 # recebe o diretorio do video , o tamanho do corte inicial em segundos e o tanho do corete final em segundos
 dir = 'c:\\Users\\ivamn\\Documents\\estudos\\sis 7p\\pexinho\\videos\\Basler_acA640-120uc__21379673__20201213_080543930.avi'
 imagens = GetFrame.GetFrames(dir,ignorarInicio,tirarFim)
-cv2.imwrite("img.jpg",imagens[1000])
+cv2.imwrite("img.jpg",imagens[1000])#printa uma imagem para fins de teste
